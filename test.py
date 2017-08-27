ec569e3b9bb1efbbb87c87d5222ce36e96de9e4e
matthewzalex <matthewzalex@gmail.com>
2017-08-27 21:13:21 +0530

    testing for identifying type of graph

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
 from graphviz import Graph
 
-dot = Graph(comment='Graph')
+dot = Graph(comment='Simple Graph')
 
-
+edges = []
 n = input("Enter no.of Edges : ")
 for i in range(0,n):
     edge = raw_input().split(' ')
     dot.edge(edge[0],edge[1],label=edge[2])
+    edges.append((edge[0],edge[1]))
 
-print(dot.source)  # doctest: +NORMALIZE_WHITESPACE
-dot.save('file.gv')
-dot.render('file.gv', view=True)
+if len(edges) != len(set(edges)):
+    dot.comment = "Multi Graph"
+
+for edge in edges:
+    if edge[0]==edge[1]:
+        dot.comment = "Pseudo Graph"
+        break
+        
+print dot.source
+dot.save('file')
+dot.render('file', view=True)
