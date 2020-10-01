8cf8da8398391ec37783464ef57a38b2967b0138
Philipg99 <32796112+Philipg99@users.noreply.github.com>
2020-10-01 14:56:42 +0530

    removed '.' form no.of

--- simple graph using networkx.py
+++ simple graph using networkx.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 g = nx.Graph()
 
-n = input("Enter no.of Edges : ")
+n = input("Enter no of Edges : ")
 for i in range(0,n):
     edge = map(int,raw_input().split(' '))
     g.add_edge(edge[0],edge[1])
@@ -16,7 +16,7 @@ plt.show()
 
 '''Sample Input
 
-Enter no.of Edges : 5
+Enter no of Edges : 5
 1 2
 2 3
 3 4
