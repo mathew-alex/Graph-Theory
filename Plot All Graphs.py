655bc257831bfbbc63d36ae7d790e65078e64afb
Philipg99 <32796112+Philipg99@users.noreply.github.com>
2020-10-01 14:55:03 +0530

    removed '.' from no.of

--- Plot All Graphs.py
+++ Plot All Graphs.py
@@ -3,7 +3,7 @@ from graphviz import Graph
 dot = Graph(comment='Graph')
 
 
-n = input("Enter no.of Edges : ")
+n = input("Enter no of Edges : ")
 for i in range(0,n):
     edge = raw_input().split(' ')
     dot.edge(edge[0],edge[1],label=edge[2])
@@ -17,7 +17,7 @@ dot.render('output.gv', view=True)
 
 '''Sample Input ,Format : node1 node2 label_of_edge 
 
-Enter no.of Edges : 5
+Enter no of Edges : 5
 1 1 a
 1 2 b
 1 2 c
